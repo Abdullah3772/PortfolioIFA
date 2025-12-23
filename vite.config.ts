865a19a11aa72defcb88cb0b29154435d966eb95
Abdullah3772 <abdullah0754520pp@gmail.com>
2025-12-23 14:41:17 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/I.F.A.Portfolio/', // <--- important for GitHub Pages
+  base: '/I.F.A.Portfolio/',
   plugins: [react()],
 });
